4f49e4c50d643d183b4392e8c94ed225c8334209
NireepV <nireep.vishnubhatla@gmail.com>
2024-09-17 23:02:08 +0930

    Websocket server with a rough implementation of a html page that can send messages.

--- server.c
+++ server.c
@@ -1,70 +1,69 @@
+#include <libwebsockets.h>
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <arpa/inet.h>
 
+#define PORT 8080
 #define BUFFER_SIZE 2048
-#define PORT_NUM 8080
-#define IP_ADDR "127.0.0.1"
 
-int main()
-{
+static int callback_websocket(struct lws *wsi, enum lws_callback_reasons reason, void *user, void *in, size_t len) {
+    switch (reason) {
+        case LWS_CALLBACK_ESTABLISHED:  // When a client connects
+            printf("Client connected\n");
+            break;
 
-    char *ip = IP_ADDR;
-    int port = PORT_NUM;
+        case LWS_CALLBACK_RECEIVE:  // When a message is received
+            printf("Client message: %s\n", (char *)in);
 
-    int server_sock, client_sock;
-    struct sockaddr_in server_addr, client_addr;
-    socklen_t addr_size;
-    char buffer[BUFFER_SIZE];
-    int n;
+            // Reply back to client
+            char response[BUFFER_SIZE];
+            snprintf(response, sizeof(response), "Server to client, hello :)");
+            lws_write(wsi, (unsigned char *)response, strlen(response), LWS_WRITE_TEXT);
+            break;
 
-    server_sock = socket(AF_INET, SOCK_STREAM, 0);
+        case LWS_CALLBACK_CLOSED:  // When a client disconnects
+            printf("Client disconnected\n");
+            break;
 
-    if (server_sock < 0)
-    {
-        perror("SOCKET ERROR");
-        exit(1);
+        default:
+            break;
     }
-    printf("SOCKET CREATED \n");
+    return 0;
+}
 
-    memset(&server_addr, '\0', sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = port;
-    server_addr.sin_addr.s_addr = inet_addr(ip);
-
-    n = bind(server_sock, (struct sockaddr *)&server_addr, sizeof(server_addr));
-    if (n < 0)
+static struct lws_protocols protocols[] = {
     {
-        perror("BIND ERROR");
-        exit(1);
-    }
-    printf("BOUND TO PORT %d \n", port);
+        "ws-protocol",
+        callback_websocket,
+        0,
+        BUFFER_SIZE,
+    },
+    { NULL, NULL, 0, 0 }  // Terminator for the protocol list
+};
 
-    listen(server_sock, 5);
-    printf("LISTENING....\n");
+int main() {
+    struct lws_context_creation_info context_info;
+    struct lws_context *context;
 
-    while (1)
-    {
-        addr_size = sizeof(client_addr);
-        client_sock = accept(server_sock, (struct sockaddr *)&client_sock, &addr_size);
-        if (client_sock > -1)
-        {
-            printf("CLIENT CONNECTED :]\n");
-        }
+    memset(&context_info, 0, sizeof(context_info));
+    context_info.port = PORT;
+    context_info.protocols = protocols;
+    context_info.gid = -1;
+    context_info.uid = -1;
 
-        bzero(buffer, BUFFER_SIZE);
-        recv(client_sock, buffer, sizeof(buffer), 0);
-        printf("Client: %s\n", buffer);
-
-        bzero(buffer, sizeof(buffer));
-        strcpy(buffer, "Server to client, hello :]");
-        printf("Server: %s\n", buffer);
-        send(client_sock, buffer, strlen(buffer), 0);
-
-        break;
+    context = lws_create_context(&context_info);
+    if (context == NULL) {
+        printf("Failed to create WebSocket context\n");
+        return -1;
     }
 
+    printf("WebSocket server started on port %d\n", PORT);
+
+    // Run the WebSocket server loop
+    while (1) {
+        lws_service(context, 1000);  // Run the event loop for 1 second intervals
+    }
+
+    lws_context_destroy(context);
     return 0;
 }
