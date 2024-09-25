352b5b4c1d3c067fd03df76c2e103242c6bfb7ad
Mr-Misterioso <a1814756@student.adelaide.edu.au>
2024-09-25 15:29:14 +0930

    Reset to try making a scrollbar from scratch

--- GUIv1.py
+++ GUIv1.py
@@ -4,33 +4,27 @@ main = tk.Tk()
 main.geometry('700x600')
 
 ### Canvases ###
-msg_canvas = tk.Canvas(main)
-etry_canvas = tk.Canvas(main)
+msg_frame = tk.Frame(main)
+etry_frame = tk.Frame(main)
 
 ### Widgets ###
-msg_etry = tk.Entry(etry_canvas)
+msg_etry = tk.Entry(etry_frame)
 msg_etry.insert(0, "Type message here")
 
 # Function to add messages
 def send():
-    if msg_etry.get() != '':
+    if msg_etry.get() != "":
         # Create the message label
-        label = tk.Label(msg_canvas, text=msg_etry.get(), background='blue')
+        label = tk.Label(msg_frame, )
         msg_etry.delete(0, tk.END)
         label.pack(side='top', anchor='e', padx='5', pady='3')
 
 # Send button
-sndbtn = tk.Button(etry_canvas, text="Send", command=send)
-
-### Scroll Bar ###
-scrollbar = tk.Scrollbar(msg_canvas, orient='vertical', command=msg_canvas.yview)
-msg_canvas.configure(yscrollcommand=scrollbar.set)
-scrollbar.pack(side='right', fill='y')
-msg_canvas.bind('<MouseWheel>', lambda event: msg_canvas.yview_scroll(int(-1 * (event.delta / 120)), 'units'))
+sndbtn = tk.Button(etry_frame, text="Send", command=send)
 
 ### Layouts ###
-etry_canvas.pack(side='bottom', fill='x')
-msg_canvas.pack(expand=True, fill='both')
+etry_frame.pack(side='bottom', fill='x')
+msg_frame.pack(expand=True, fill='both')
 
 # Widget Layout #
 sndbtn.pack(side='right', ipady=4)
