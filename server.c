f78b3dac0408a83454747f83d1f06af6c2b76b23
NireepV <109134856+NireepV@users.noreply.github.com>
2024-09-26 08:58:33 +0000

    Server Now responds to different types of messages, Hello messages, client-list requests, Public Chat messages, etc. Although the implementations for these responses is still incomplete, it is merely a functioning prototype. Added a dynamically alocated user_list(client_list), there needs to be some changes in adding and removing users.

--- server.c
+++ server.c
@@ -11,22 +11,158 @@
 #define PORT 8080
 #define BUFFER_SIZE 2048
 
+typedef struct IPAddr{
+  char *ip;
+  int port;
+} addr;
+
+// Setting Up Client List
+typedef struct client{
+    addr address;
+    int public_key;
+} User;
+
+User *user_list;
+int size = 1;
+
+//Adding Clients into Client List
+void add_users(int key, char *address, int port){
+    for(int i = 0; i < size; i++){
+        if(user_list[i].address.ip == NULL){
+            user_list[i].address.ip = address;
+            user_list[i].address.port = port;
+            user_list[i].public_key = key;
+            size++; //increment the size of the list after adding a new client
+            
+            user_list = realloc(user_list, size * sizeof(User)); //There should always be 1 additional empty user
+            break;
+        }
+    }
+}
+
+// WebSocket protocols
+void handle_hello_messages(cJSON *data){
+    // Get Client's Public Key Like This:    
+    cJSON *key = cJSON_GetObjectItemCaseSensitive(data, "public_key");
+    printf("THIS IS THE PUBLIC KEY : %d\n", key->valueint);
+    
+    // Adding Client to the Client List
+    add_users(key->valueint, "127.0.0.1", PORT);
+}
+
+void handle_chat_messages(cJSON *data){
+    // TO DO
+}
+
+void handle_public_chat_messages(cJSON *data){
+    // Get Client's Public Key Like This:    
+    cJSON *sender = cJSON_GetObjectItemCaseSensitive(data, "sender");
+    cJSON *message = cJSON_GetObjectItemCaseSensitive(data, "message");
+    
+    printf("THE SENDERS FINGERPRINT : %d\n", sender->valueint);
+    printf("THE SENDERS PUBLIC CHAT MESSAGE : %s\n", message->valuestring);
+}
+
+char* handle_client_list_request(cJSON *data) {
+    // Create root object
+    cJSON *response = cJSON_CreateObject();
+    
+    // Add type field
+    cJSON_AddStringToObject(response, "type", "client_list");
+    
+    // Create the servers array
+    cJSON *servers_array = cJSON_CreateArray();
+    
+    // Iterate over the user list to create server entries
+    for (int i = 0; i < size; i++) {
+        // Create a server object
+        cJSON *server = cJSON_CreateObject();
+        cJSON_AddStringToObject(server, "address", user_list[i].address.ip); // Add the address of the server
+        
+        // Create clients array for each server
+        cJSON *clients_array = cJSON_CreateArray();
+        
+        // Add the client's public key to the clients array
+        cJSON_AddItemToArray(clients_array, cJSON_CreateNumber(user_list[i].public_key));
+        
+        // Attach clients array to the server object
+        cJSON_AddItemToObject(server, "clients", clients_array);
+        
+        // Add this server object to the servers array
+        cJSON_AddItemToArray(servers_array, server);
+    }
+    
+    cJSON_AddItemToObject(response, "servers", servers_array);
+    
+    char *json_string = cJSON_Print(response);
+
+    cJSON_Delete(response);
+    
+    return json_string;
+}
+
 // WebSocket callback function
 static int callback_websocket(struct lws *wsi, enum lws_callback_reasons reason, void *user, void *in, size_t len) {
+    
+    char *buffer;
+    char response[BUFFER_SIZE];
+    
     switch (reason) {
         case LWS_CALLBACK_ESTABLISHED:  // When a client connects
             printf("Client connected\n");
             break;
 
         case LWS_CALLBACK_RECEIVE:  // When a message is received
-            // Print the received message
-            printf("Client message: %.*s\n", (int)len, (char *)in);
-
-            // Respond to the client
-            char response[BUFFER_SIZE];
-            snprintf(response, sizeof(response), "Server to client, hello :)");
-            lws_write(wsi, (unsigned char *)response, strlen(response), LWS_WRITE_TEXT);
-            break;
+        buffer = (char *)in;
+        
+        cJSON *json = cJSON_Parse(buffer);
+        if (json == NULL)
+        {
+            printf("Error parsing JSON: %s\n", cJSON_GetErrorPtr());
+        }
+        else
+        {
+            cJSON *jData= cJSON_GetObjectItemCaseSensitive(json, "type"); 
+            
+            if(strcmp(jData->valuestring,"signed_data") == 0)
+            {  // checks to see if it is a standard message or a client list request
+                jData = cJSON_GetObjectItemCaseSensitive(json, "data");
+                cJSON *jType= cJSON_GetObjectItemCaseSensitive(jData, "type");
+                
+                if(strcmp(jType->valuestring , "hello") == 0)
+                {
+                    handle_hello_messages(jData);
+                }
+                else if(strcmp(jType->valuestring , "chat") == 0)
+                {
+                    printf("MESSAGE RECIEVED : CHAT\n");
+                    
+                    // Respond to the Client
+                    snprintf(response, sizeof(response), "Server is responding to a Private Message ^_^");
+                    lws_write(wsi, (unsigned char *)response, strlen(response), LWS_WRITE_TEXT);
+                }
+                else if(strcmp(jType->valuestring , "public_chat") == 0)
+                {
+                    handle_public_chat_messages(jData);
+                    
+                    // Respond to the Client
+                    snprintf(response, sizeof(response), "Server is responding to a Public Message O_O");
+                    lws_write(wsi, (unsigned char *)response, strlen(response), LWS_WRITE_TEXT);
+                }
+            }
+            else if(strcmp(jData->valuestring,"client_list_request") == 0)
+            {
+                // Get Client List from Server Formatted in JSON
+                char* json_string = handle_client_list_request(jData);
+                
+                // Respond to the Client
+                snprintf(response, sizeof(response), "%s", json_string);
+                lws_write(wsi, (unsigned char *)response, strlen(response), LWS_WRITE_TEXT);
+                printf("SENT CLIENT LIST\n");
+            }
+        }
+        
+        break;
 
         case LWS_CALLBACK_CLOSED:  // When a client disconnects
             printf("Client disconnected\n");
@@ -38,7 +174,6 @@ static int callback_websocket(struct lws *wsi, enum lws_callback_reasons reason,
     return 0;
 }
 
-// WebSocket protocols
 static struct lws_protocols protocols[] = {
     {
         "ws-protocol",   // Protocol name (must match in the client-side WebSocket)
@@ -69,6 +204,9 @@ int main() {
     }
 
     printf("WebSocket server started on port %d\n", PORT);
+    
+    // Allocating Memory for Client List
+    user_list = (User *) malloc(size * sizeof(User));
 
     // Run the WebSocket server loop
     while (1) {
@@ -77,6 +215,8 @@ int main() {
 
     // Clean up the WebSocket context
     lws_context_destroy(context);
+    
+    free(user_list);
 
     return 0;
 }
